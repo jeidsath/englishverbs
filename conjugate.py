a729572d459a8064dfe4f49169b6235cd1831541
Joel Eidsath <jeidsath@gmail.com>
2017-05-16 07:34:44 -0700

    First attempt

---
+++ conjugate.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+# person: first, second, third
+# number: singular, plural
+# voice: active, passive
+# tense: past, present, future,
+# aspect: unmarked, progressive, perfect
+# mood: indicative, imperative, subjunctive, interrogative
+
+
+import re
+import irregulars
+
+CONSONANTS = list('bcdfghjklmnpqrstvwxz')
+DOUBLED_CONSONANTS = list('bcdfghjklmnpqrstz')
+VOWELS = list('aeiouy')
+CONSONANTS_PATTERN = "[" + ''.join(CONSONANTS) + "]"
+DOUBLED_CONSONANTS_PATTERN = "[" + ''.join(DOUBLED_CONSONANTS) + "]"
+VOWELS_PATTERN = "[" + ''.join(VOWELS) + "]"
+FORMS = {}
+
+
+def construct_forms():
+    global FORMS
+    if FORMS != {}:
+        return FORMS
+
+    forms = {}
+    for vv in ['active', 'passive']:
+        forms[vv] = {}
+
+    forms['active']['unmarked'] = [':verb']
+    forms['active']['progressive'] = [':be', ':present_participle']
+    forms['active']['perfect'] = [':have', ':past_participle']
+    forms['passive']['unmarked'] = [':be', ':past_participle']
+    forms['passive']['progressive'] = [':be', 'being', ':past_participle']
+    forms['passive']['perfect'] = [':have', 'been', ':past_participle']
+
+    # TODO:  future perfect passive progressive
+    # I will have been being released
+
+    FORMS = forms
+    return FORMS
+
+
+def conjugate(infinitive='be', person='third', number='singular',
+              voice='active', tense='present', aspect='unmarked',
+              mood='indicative'):
+
+    form = construct_forms()[voice][aspect]
+    return resolve(form, infinitive, person, number, voice, tense, aspect, mood)
+
+
+def resolve(form, infinitive, person, number, voice, tense, aspect, mood):
+    output = []
+    for ff in form:
+        if ff == ':verb':
+            output.append(verb(infinitive, tense, person, number, mood))
+        if ff == ':infinitive':
+            output.append(infinitive)
+        if ff == ':present_participle':
+            output.append(present_participle(infinitive))
+        if ff == ':past_participle':
+            output.append(past_participle(infinitive))
+        if ff == ':have':
+            output.append(verb('have', tense, person, number, mood))
+        if ff == ':be':
+            output.append(verb('be', tense, person, number, mood))
+
+    return ' '.join(output)
+
+
+def is_single_term_consonant(infinitive):
+    for ww in irregulars.SINGLE_TERMINAL_CONSONANT:
+        if infinitive.endswith(ww):
+            return True
+    return False
+
+
+def present_participle(infinitive):
+    if infinitive == 'be':
+        return irregulars.BE['present_participle']
+    if infinitive == 'have':
+        return irregulars.HAVE['present_participle']
+    cvc_regex = re.compile(r'.*' + CONSONANTS_PATTERN + VOWELS_PATTERN + CONSONANTS_PATTERN + '$')
+    if cvc_regex.match(infinitive) and not is_single_term_consonant(infinitive):
+        return present_participle_with_doubled_terminal_consonant_for(infinitive)
+    if re.match(r'.*c$', infinitive):
+        return infinitive + 'king'
+    ing_matches = [r'.*ye$', r'.*oe$', r'.*nge$', r'.*ee$']
+    for rr in ing_matches:
+        if re.match(rr, infinitive):
+            return infinitive + 'ing'
+    if re.match(r'.*ie$', infinitive):
+        return infinitive[0:-1] + 'ying'
+    if re.match(r'.*e$', infinitive):
+        return infinitive[0:-1] + 'ing'
+    return infinitive + 'ing'
+
+
+def preterite_for(infinitive):
+    if infinitive in irregulars.IRREGULARS.keys():
+        return irregulars.IRREGULARS[infinitive][0]
+    cvc_regex = re.compile(r'.*' + CONSONANTS_PATTERN + VOWELS_PATTERN + CONSONANTS_PATTERN + '$')
+    if cvc_regex.match(infinitive) and not is_single_term_consonant(infinitive):
+        return regular_preterite_with_doubled_terminal_consonant_for(infinitive)
+    d_matches = [r'.*' + CONSONANTS_PATTERN + 'e$', r'.*ye$', r'.*oe$', r'.*nge$', r'.*ee$']
+    for rr in d_matches:
+        if re.match(rr, infinitive):
+            return infinitive + 'd'
+    if re.match(r'.*' + CONSONANTS_PATTERN + 'y$', infinitive):
+        return infinitive[0:-1] + 'ied'
+    return infinitive + 'ed'
+
+
+def regular_preterite_with_doubled_terminal_consonant_for(infinitive):
+    preterite_for(infinitive + infinitive[-1])
+
+
+def present_participle_with_doubled_terminal_consonant_for(infinitive):
+    return present_participle(infinitive + infinitive[-1])
+
+
+def present_third_person_singular_form_for(infinitive):
+    if re.match(r'.*' + CONSONANTS_PATTERN + 'y$', infinitive):
+        return infinitive[0:-1] + 'ies'
+    es_matches = [r'.*' + CONSONANTS_PATTERN + 'o$', r'.*ss$', r'.*sh$', r'.*ch$', r'.*zz$', r'.*x$']
+    for rr in es_matches:
+        if re.match(rr, infinitive):
+            return infinitive + 'es'
+    if re.match(r'.*[^s]s$', infinitive):
+        return infinitive + 'ses'
+    return infinitive + 's'
+
+
+def past_participle(infinitive):
+    if infinitive == 'be':
+        return irregulars.BE['past_participle']
+    if infinitive == 'have':
+        return irregulars.HAVE['past_participle']
+    if infinitive in irregulars.IRREGULARS.keys():
+        return irregulars.IRREGULARS[infinitive][1]
+    return preterite_for(infinitive)
+
+
+def verb(infinitive, tense, person, number, mood):
+    # TODO: mood
+    if tense == 'past' or tense == 'present':
+        if infinitive == 'be':
+            return irregulars.BE['finite'][tense][number][person]
+        if infinitive == 'have':
+            return irregulars.HAVE['finite'][tense][number][person]
+
+    if tense == 'past':
+        return preterite_for(infinitive)
+
+    if tense == 'present':
+        if person == 'third' and number == 'singular':
+            return present_third_person_singular_form_for(infinitive)
+        return infinitive
+
+    if tense == 'future':
+        return 'will ' + infinitive
---
+++ irregulars.py
@@ -0,0 +1,470 @@
+BE = {}
+BE['finite'] = {}
+BE['finite']['present'] = {}
+BE['finite']['present']['singular'] = {}
+BE['finite']['present']['plural'] = {}
+BE['finite']['past'] = {}
+BE['finite']['past']['singular'] = {}
+BE['finite']['past']['plural'] = {}
+
+
+BE['finite']['present']['singular']['first'] = 'am'
+BE['finite']['present']['singular']['second'] = 'are'
+BE['finite']['present']['singular']['third'] = 'is'
+BE['finite']['present']['plural']['first'] = 'are'
+BE['finite']['present']['plural']['second'] = 'are'
+BE['finite']['present']['plural']['third'] = 'are'
+
+BE['finite']['past']['singular']['first'] = 'was'
+BE['finite']['past']['singular']['second'] = 'were'
+BE['finite']['past']['singular']['third'] = 'was'
+BE['finite']['past']['plural']['first'] = 'were'
+BE['finite']['past']['plural']['second'] = 'were'
+BE['finite']['past']['plural']['third'] = 'were'
+
+BE['present_participle'] = 'being'
+BE['past_participle'] = 'been'
+
+HAVE = {}
+HAVE['finite'] = {}
+HAVE['finite']['present'] = {}
+HAVE['finite']['present']['singular'] = {}
+HAVE['finite']['present']['plural'] = {}
+HAVE['finite']['past'] = {}
+HAVE['finite']['past']['singular'] = {}
+HAVE['finite']['past']['plural'] = {}
+
+HAVE['finite']['present']['singular']['first'] = 'have'
+HAVE['finite']['present']['singular']['second'] = 'have'
+HAVE['finite']['present']['singular']['third'] = 'has'
+HAVE['finite']['present']['plural']['first'] = 'have'
+HAVE['finite']['present']['plural']['second'] = 'have'
+HAVE['finite']['present']['plural']['third'] = 'have'
+
+HAVE['finite']['past']['singular']['first'] = 'had'
+HAVE['finite']['past']['singular']['second'] = 'had'
+HAVE['finite']['past']['singular']['third'] = 'had'
+HAVE['finite']['past']['plural']['first'] = 'had'
+HAVE['finite']['past']['plural']['second'] = 'had'
+HAVE['finite']['past']['plural']['third'] = 'had'
+
+HAVE['present_participle'] = 'having'
+HAVE['past_participle'] = 'had'
+
+# http://cpansearch.perl.org/src/RWG/Lingua-EN-Conjugate-0.308/lib/Lingua/EN/Conjugate.pm
+
+IRREGULARS = {}
+IRREGULARS['awake'] = ['awoke', 'awoken']
+IRREGULARS['bear'] = ['bore', 'born']
+IRREGULARS['beat'] = ['beat', 'beat']
+IRREGULARS['become'] = ['became', 'become']
+IRREGULARS['begin'] = ['began', 'begun']
+IRREGULARS['bend'] = ['bent', 'bent']
+IRREGULARS['beset'] = ['beset', 'beset']
+IRREGULARS['bet'] = ['bet', 'bet']
+IRREGULARS['bid'] = ['bid', 'bid']
+IRREGULARS['bind'] = ['bound', 'bound']
+IRREGULARS['bite'] = ['bit', 'bitten']
+IRREGULARS['bleed'] = ['bled', 'bled']
+IRREGULARS['blow'] = ['blew', 'blown']
+IRREGULARS['break'] = ['broke', 'broken']
+IRREGULARS['breed'] = ['bred', 'bred']
+IRREGULARS['bring'] = ['brought', 'brought']
+IRREGULARS['broadcast'] = ['broadcast', 'broadcast']
+IRREGULARS['build'] = ['built', 'built']
+IRREGULARS['burn'] = ['burned', 'burned']
+IRREGULARS['burst'] = ['burst', 'burst']
+IRREGULARS['buy'] = ['bought', 'bought']
+IRREGULARS['cast'] = ['cast', 'cast']
+IRREGULARS['catch'] = ['caught', 'caught']
+IRREGULARS['choose'] = ['chose', 'chosen']
+IRREGULARS['cling'] = ['clung', 'clung']
+IRREGULARS['come'] = ['came', 'come']
+IRREGULARS['cost'] = ['cost', 'cost']
+IRREGULARS['creep'] = ['crept', 'crept']
+IRREGULARS['cut'] = ['cut', 'cut']
+IRREGULARS['deal'] = ['dealt', 'dealt']
+IRREGULARS['dig'] = ['dug', 'dug']
+IRREGULARS['dive'] = ['dove', 'dived']
+IRREGULARS['do'] = ['did', 'done']
+IRREGULARS['draw'] = ['drew', 'drawn']
+IRREGULARS['dream'] = ['dreamed', 'dreamed']
+IRREGULARS['drive'] = ['drove', 'driven']
+IRREGULARS['drink'] = ['drank', 'drunk']
+IRREGULARS['eat'] = ['ate', 'eaten']
+IRREGULARS['fall'] = ['fell', 'fallen']
+IRREGULARS['feed'] = ['fed', 'fed']
+IRREGULARS['feel'] = ['felt', 'felt']
+IRREGULARS['fight'] = ['fought', 'fought']
+IRREGULARS['find'] = ['found', 'found']
+IRREGULARS['fit'] = ['fit', 'fit']
+IRREGULARS['flee'] = ['fled', 'fled']
+IRREGULARS['fling'] = ['flung', 'flung']
+IRREGULARS['fly'] = ['flew', 'flown']
+IRREGULARS['forbid'] = ['forbade', 'forbidden']
+IRREGULARS['forget'] = ['forgot', 'forgotten']
+IRREGULARS['forego'] = ['forewent', 'foregone']
+IRREGULARS['forgo'] = ['forwent', 'forgone']
+IRREGULARS['forgive'] = ['forgave', 'forgiven']
+IRREGULARS['forsake'] = ['forsook', 'forsaken']
+IRREGULARS['freeze'] = ['froze', 'frozen']
+IRREGULARS['get'] = ['got', 'gotten']
+IRREGULARS['give'] = ['gave', 'given']
+IRREGULARS['go'] = ['went', 'gone']
+IRREGULARS['grind'] = ['ground', 'ground']
+IRREGULARS['grow'] = ['grew', 'grown']
+IRREGULARS['hang'] = ['hung', 'hung']
+IRREGULARS['hear'] = ['heard', 'heard']
+IRREGULARS['hide'] = ['hid', 'hidden']
+IRREGULARS['hit'] = ['hit', 'hit']
+IRREGULARS['hold'] = ['held', 'held']
+IRREGULARS['hurt'] = ['hurt', 'hurt']
+IRREGULARS['keep'] = ['kept', 'kept']
+IRREGULARS['kneel'] = ['knelt', 'knelt']
+IRREGULARS['knit'] = ['knit', 'knit']
+IRREGULARS['know'] = ['knew', 'known']
+IRREGULARS['lay'] = ['laid', 'laid']
+IRREGULARS['lead'] = ['led', 'led']
+IRREGULARS['leap'] = ['leaped', 'leaped']
+IRREGULARS['learn'] = ['learned', 'learned']
+IRREGULARS['leave'] = ['left', 'left']
+IRREGULARS['lend'] = ['lent', 'lent']
+IRREGULARS['let'] = ['let', 'let']
+IRREGULARS['lie'] = ['lay', 'lain']
+IRREGULARS['light'] = ['lit', 'lighted']
+IRREGULARS['lose'] = ['lost', 'lost']
+IRREGULARS['make'] = ['made', 'made']
+IRREGULARS['mean'] = ['meant', 'meant']
+IRREGULARS['meet'] = ['met', 'met']
+IRREGULARS['misspell'] = ['misspelled', 'misspelled']
+IRREGULARS['mistake'] = ['mistook', 'mistaken']
+IRREGULARS['mow'] = ['mowed', 'mowed']
+IRREGULARS['overcome'] = ['overcame', 'overcome']
+IRREGULARS['overdo'] = ['overdid', 'overdone']
+IRREGULARS['overtake'] = ['overtook', 'overtaken']
+IRREGULARS['overthrow'] = ['overthrew', 'overthrown']
+IRREGULARS['pay'] = ['paid', 'paid']
+IRREGULARS['plead'] = ['pled', 'pled']
+IRREGULARS['prove'] = ['proved', 'proved']
+IRREGULARS['put'] = ['put', 'put']
+IRREGULARS['quit'] = ['quit', 'quit']
+IRREGULARS['read'] = ['read', 'read']
+IRREGULARS['reset'] = ['reset', 'reset']
+IRREGULARS['rid'] = ['rid', 'rid']
+IRREGULARS['ride'] = ['rode', 'ridden']
+IRREGULARS['ring'] = ['rang', 'rung']
+IRREGULARS['rise'] = ['rose', 'risen']
+IRREGULARS['run'] = ['ran', 'run']
+IRREGULARS['saw'] = ['sawed', 'sawed']
+IRREGULARS['say'] = ['said', 'said']
+IRREGULARS['see'] = ['saw', 'seen']
+IRREGULARS['seek'] = ['sought', 'sought']
+IRREGULARS['sell'] = ['sold', 'sold']
+IRREGULARS['send'] = ['sent', 'sent']
+IRREGULARS['set'] = ['set', 'set']
+IRREGULARS['sew'] = ['sewed', 'sewed']
+IRREGULARS['shake'] = ['shook', 'shaken']
+IRREGULARS['shave'] = ['shaved', 'shaved']
+IRREGULARS['shear'] = ['shore', 'shorn']
+IRREGULARS['shed'] = ['shed', 'shed']
+IRREGULARS['shine'] = ['shone', 'shone']
+IRREGULARS['shoe'] = ['shoed', 'shoed']
+IRREGULARS['shoot'] = ['shot', 'shot']
+IRREGULARS['show'] = ['showed', 'showed']
+IRREGULARS['shrink'] = ['shrank', 'shrunk']
+IRREGULARS['shut'] = ['shut', 'shut']
+IRREGULARS['sing'] = ['sang', 'sung']
+IRREGULARS['sink'] = ['sank', 'sunk']
+IRREGULARS['sit'] = ['sat', 'sat']
+IRREGULARS['sleep'] = ['slept', 'slept']
+IRREGULARS['slay'] = ['slew', 'slain']
+IRREGULARS['slide'] = ['slid', 'slid']
+IRREGULARS['sling'] = ['slung', 'slung']
+IRREGULARS['slit'] = ['slit', 'slit']
+IRREGULARS['smite'] = ['smote', 'smitten']
+IRREGULARS['sow'] = ['sowed', 'sowed']
+IRREGULARS['speak'] = ['spoke', 'spoken']
+IRREGULARS['speed'] = ['sped', 'sped']
+IRREGULARS['spend'] = ['spent', 'spent']
+IRREGULARS['spill'] = ['spilled', 'spilled']
+IRREGULARS['spin'] = ['spun', 'spun']
+IRREGULARS['spit'] = ['spit', 'spit']
+IRREGULARS['split'] = ['split', 'split']
+IRREGULARS['spread'] = ['spread', 'spread']
+IRREGULARS['spring'] = ['sprang', 'sprung']
+IRREGULARS['stand'] = ['stood', 'stood']
+IRREGULARS['steal'] = ['stole', 'stolen']
+IRREGULARS['stick'] = ['stuck', 'stuck']
+IRREGULARS['sting'] = ['stung', 'stung']
+IRREGULARS['stink'] = ['stank', 'stunk']
+IRREGULARS['stride'] = ['strod', 'stridden']
+IRREGULARS['strike'] = ['struck', 'struck']
+IRREGULARS['string'] = ['strung', 'strung']
+IRREGULARS['strive'] = ['strove', 'striven']
+IRREGULARS['swear'] = ['swore', 'sworn']
+IRREGULARS['sweep'] = ['swept', 'swept']
+IRREGULARS['swell'] = ['swelled', 'swelled']
+IRREGULARS['swim'] = ['swam', 'swum']
+IRREGULARS['swing'] = ['swung', 'swung']
+IRREGULARS['take'] = ['took', 'taken']
+IRREGULARS['teach'] = ['taught', 'taught']
+IRREGULARS['tear'] = ['tore', 'torn']
+IRREGULARS['tell'] = ['told', 'told']
+IRREGULARS['think'] = ['thought', 'thought']
+IRREGULARS['thrive'] = ['thrived', 'thrived']
+IRREGULARS['throw'] = ['threw', 'thrown']
+IRREGULARS['thrust'] = ['thrust', 'thrust']
+IRREGULARS['tread'] = ['trod', 'trodden']
+IRREGULARS['understand'] = ['understood', 'understood']
+IRREGULARS['uphold'] = ['upheld', 'upheld']
+IRREGULARS['upset'] = ['upset', 'upset']
+IRREGULARS['wake'] = ['woke', 'woken']
+IRREGULARS['wear'] = ['wore', 'worn']
+IRREGULARS['weave'] = ['wove', 'woven']
+IRREGULARS['wed'] = ['wed', 'wed']
+IRREGULARS['weep'] = ['wept', 'wept']
+IRREGULARS['wind'] = ['wound', 'wound']
+IRREGULARS['win'] = ['won', 'won']
+IRREGULARS['withhold'] = ['withheld', 'withheld']
+IRREGULARS['withstand'] = ['withstood', 'withstood']
+IRREGULARS['wring'] = ['wrung', 'wrung']
+IRREGULARS['write'] = ['wrote', 'written']
+
+SINGLE_TERMINAL_CONSONANT = [ 'abandon',
+                              'accouter',
+                              'accredit',
+                              'adhibit',
+                              'administer',
+                              'alter',
+                              'anchor',
+                              'answer',
+                              'attrit',
+                              'audit',
+                              'author',
+                              'ballot',
+                              'banner',
+                              'batten',
+                              'bedizen',
+                              'bespatter',
+                              'betoken',
+                              'bewilder',
+                              'billet',
+                              'blacken',
+                              'blither',
+                              'blossom',
+                              'bother',
+                              'brighten',
+                              'broaden',
+                              'broider',
+                              'burden',
+                              'caparison',
+                              'catalog',
+                              'censor',
+                              'center',
+                              'charter',
+                              'chatter',
+                              'cheapen',
+                              'chipper',
+                              'chirrup',
+                              'christen',
+                              'clobber',
+                              'cluster',
+                              'coarsen',
+                              'cocker',
+                              'coedit',
+                              'cohabit',
+                              'color',
+                              'concenter',
+                              'corner',
+                              'cover',
+                              'covet',
+                              'cower',
+                              'credit',
+                              'custom',
+                              'dampen',
+                              'deafen',
+                              'decipher',
+                              'deflower',
+                              'delimit',
+                              'deposit',
+                              'develop',
+                              'differ',
+                              'disaccustom',
+                              'discover',
+                              'discredit',
+                              'disencumber',
+                              'dishearten',
+                              'disinherit',
+                              'dismember',
+                              'dispirit',
+                              'dither',
+                              'dizen',
+                              'dodder',
+                              'edit',
+                              'elicit',
+                              'embitter',
+                              'embolden',
+                              'embosom',
+                              'embower',
+                              'empoison',
+                              'empower',
+                              'enamor',
+                              'encipher',
+                              'encounter',
+                              'endanger',
+                              'enfetter',
+                              'engender',
+                              'enlighten',
+                              'enter',
+                              'envelop',
+                              'envenom',
+                              'environ',
+                              'exhibit',
+                              'exit',
+                              'fasten',
+                              'fatten',
+                              'feather',
+                              'fester',
+                              'filter',
+                              'flatten',
+                              'flatter',
+                              'flounder',
+                              'fluster',
+                              'flutter',
+                              'foreshorten',
+                              'founder',
+                              'fritter',
+                              'gammon',
+                              'gather',
+                              'gladden',
+                              'glimmer',
+                              'glisten',
+                              'glower',
+                              'greaten',
+                              'hamper',
+                              'hanker',
+                              'happen',
+                              'harden',
+                              'harken',
+                              'hasten',
+                              'hearten',
+                              'hoarsen',
+                              'honor',
+                              'imprison',
+                              'inhabit',
+                              'inhibit',
+                              'inspirit',
+                              'interpret',
+                              'iron',
+                              'know',
+                              'laten',
+                              'launder',
+                              'lengthen',
+                              'liken',
+                              'limber',
+                              'limit',
+                              'linger',
+                              'litter',
+                              'liven',
+                              'loiter',
+                              'lollop',
+                              'louden',
+                              'lower',
+                              'lumber',
+                              'madden',
+                              'malinger',
+                              'market',
+                              'matter',
+                              'misinterpret',
+                              'misremember',
+                              'monitor',
+                              'moulder',
+                              'murder',
+                              'murmur',
+                              'muster',
+                              'number',
+                              'offer',
+                              'open',
+                              'order',
+                              'outmaneuver',
+                              'overmaster',
+                              'pamper',
+                              'pilot',
+                              'pivot',
+                              'plaster',
+                              'plunder',
+                              'powder',
+                              'power',
+                              'prohibit',
+                              'reckon',
+                              'reconsider',
+                              'recover',
+                              'redden',
+                              'redeliver',
+                              'register',
+                              'rejigger',
+                              'remember',
+                              'renumber',
+                              'reopen',
+                              'reposit',
+                              'rewaken',
+                              'richen',
+                              'roister',
+                              'roughen',
+                              'sadden',
+                              'savor',
+                              'scatter',
+                              'scupper',
+                              'sharpen',
+                              'shatter',
+                              'shelter',
+                              'shimmer',
+                              'shiver',
+                              'shorten',
+                              'shower',
+                              'sicken',
+                              'smolder',
+                              'smoothen',
+                              'soften',
+                              'solicit',
+                              'squander',
+                              'stagger',
+                              'stiffen',
+                              'stopper',
+                              'stouten',
+                              'straiten',
+                              'strengthen',
+                              'stutter',
+                              'suffer',
+                              'sugar',
+                              'summon',
+                              'surrender',
+                              'swelter',
+                              'sypher',
+                              'tamper',
+                              'tauten',
+                              'tender',
+                              'thicken',
+                              'threaten',
+                              'thunder',
+                              'totter',
+                              'toughen',
+                              'tower',
+                              'transit',
+                              'trigger',
+                              'tucker',
+                              'unburden',
+                              'uncover',
+                              'unfetter',
+                              'unloosen',
+                              'upholster',
+                              'utter',
+                              'visit',
+                              'vomit',
+                              'wander',
+                              'water',
+                              'weaken',
+                              'whiten',
+                              'winter',
+                              'wonder',
+                              'worsen']
